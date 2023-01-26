56462bc3f92a9fca3eeb8191d47cbfd703f725df
Pin Yuan <pin100705@gmail.com>
2023-01-26 14:04:00 +0800

    modify home DB Tools to Tool

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+
 # Register your models here.
+
+# The Database we register to backend control
+
+
+from .models import Tool , CurAlarm
+
+@admin.register(Tools)
+class ToolsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(CurAlarm)
+class CurAlarmAdmin(admin.ModelAdmin):
+    pass
--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Tools(models.Model):
+class Tool(models.Model):
     id_primary = models.IntegerField(primary_key=True)
     tools = models.CharField(max_length=20)
     fabs = models.CharField(max_length=20)
